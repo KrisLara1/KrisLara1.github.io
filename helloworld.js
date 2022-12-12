a4c926811b6f635162d3f02d52a956969aacd2a8
KrisLara1 <KLara1@caldwell.edu>
2022-12-12 16:30:38 -0500

    Final

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,32 @@
-function hello(){
-    alert('hello user!')
-   }
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+
+
+function openskill(evt, skillName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(skillName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
